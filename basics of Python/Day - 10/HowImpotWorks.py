66a9465c459a2bd8b1064c568db2628ba09d2816
Tirthesh <92018039+Tirthesh1843@users.noreply.github.com>
2026-06-03 23:41:43 +0530

    Day 10: Revised Python fundamentals and learned imports and virtual environments

---
+++ basics of Python/Day - 10/HowImpotWorks.py
@@ -0,0 +1,11 @@
+#Day - 44
+
+# How import works in Python
+
+import pandas as pd
+print (pd.__version__)
+
+from math import sqrt, factorial, pi
+print (sqrt(16))
+print (factorial(5))
+print (pi)
---
+++ basics of Python/Day - 10/VirtualEnvironment.py
@@ -0,0 +1,16 @@
+# run python -m venv <folder_name> to create a virtual environment in the current directory. This will create a new folder with the name specified in <folder_name> that contains the virtual environment.
+
+# run <folder_name>\Scripts\activate to activate the virtual environment. This will change the command prompt to indicate that the virtual environment is active.
+# run <folder_name>\Scripts\deactivate to deactivate the virtual environment. This will change the command prompt back to normal.
+
+# run pip install <package_name> to install a package in the virtual environment. This will install the package in the virtual environment and make it available for use in your Python scripts.
+
+# run pip freeze to list all the packages installed in the virtual environment. This will show the name and version of each package installed in the virtual environment.
+
+# run pip uninstall <package_name> to remove a package from the virtual environment. This will remove the package from the virtual environment and make it unavailable for use in your Python scripts.
+
+# run pip list to list all the packages installed in the virtual environment. This will show the name and version of each package installed in the virtual environment.
+
+# run pip install --upgrade <package_name> to upgrade a package in the virtual environment. This will install the latest version of the package and make it available for use in your Python scripts.
+
+# run pip install --upgrade pip to upgrade pip itself in the virtual environment. This will make sure that pip is up to date and can install new packages correctly.
