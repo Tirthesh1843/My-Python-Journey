44e0689ec0c9fb47c0b4957cb880bc8c056e3c50
Tirthesh <92018039+Tirthesh1843@users.noreply.github.com>
2026-06-04 23:03:52 +0530

    Day 11: Learned the __name__ == '__main__' pattern in Python

---
+++ basics of Python/Day -11/if==main.py
@@ -0,0 +1,5 @@
+#Day - 45
+
+import tirthesh
+# print(__name__)
+tirthesh.welcome()
---
+++ basics of Python/Day -11/tirthesh.py
@@ -0,0 +1,5 @@
+def welcome():
+    print("Welcome to the world of Python programming!")
+print(__name__)
+if __name__ == "__main__":
+    welcome()
