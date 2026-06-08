42c77d233897d105a9bf2ab62059feb84126347a
Tirthesh <92018039+Tirthesh1843@users.noreply.github.com>
2026-06-08 23:25:51 +0530

    Learned map(), filter(), reduce(), and practiced Python fundamentals

--- Python daily paractice/Day-13.py
+++ Python daily paractice/Day-13.py
@@ -1,5 +1,21 @@
-x = 0
-if x:
-    print("x is true")
-else:
-    print("x is false")
+# x = 0
+# if x:
+#     print("x is true")
+# else:
+#     print("x is false")
+
+sentence = "Tirthesh and Priya are learning Python"
+
+# 1. Print first word
+# 2. Print last word
+# 3. Convert whole sentence to uppercase
+# 4. Count letter 'a'
+# 5. Replace Python with AI
+
+first_Word = sentence.split()[0]
+last_word = sentence.split()[-1]
+upper_case = sentence.upper()
+Count_a = sentence.count("a")
+replace = sentence.replace("Python", "AI")
+
+print (first_Word, last_word, upper_case, Count_a, replace, end="\n")
--- basics of Python/Day - 13/MapFilter&Reduce.py
+++ basics of Python/Day - 13/MapFilter&Reduce.py
@@ -10,3 +10,10 @@ l = [1, 2, 4, 6, 4, 5]
 # for item in l:
 #     newl.append(cube(item))
 # print(newl)
+
+newl = list(map(cube, l)) #
+print (l, " -> ", newl)
+
+name = "Tirthesh"
+print(name[0:4])
+print(name[-4:-1])
